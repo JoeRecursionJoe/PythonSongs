0edc8da9b572984dc7dfc0161c3bb90fe4d2123f
JoeRecursionJoe <Joe@Riedley.net>
2023-09-20 02:31:36 +0000

    Updates Moneyfornothing

--- moneyfornothing.py
+++ moneyfornothing.py
@@ -1,2 +1,65 @@
 # by Joe Riedley
 
+import os
+
+
+class MTV:
+	
+  def __init__(self):
+    self.want = ""
+    print("Money For Nothing")
+  
+  
+  def look(self, what):
+    return print(f"look at them {what}")
+		
+		
+  def play(self):
+    self.want = ""
+    self.want = "MTV"
+    self.want = ""
+    self.want = "MTV"
+    self.want = ""
+    self.want = "MTV"
+    self.want = ""
+    self.want = "MTV"
+          
+    self.look("YoYo's'")
+          
+    do = True
+          
+          
+    if MTV.play == ("Guitar"):
+      working = False
+      do = True
+      moneycost = 0
+      chickscost = 0
+        
+      working = False
+      do = True
+        
+      guysdumb = False
+      littlefinger = "blister"
+      thumb = "blister"
+      
+    else:
+      
+      try:
+        os.system("install microwave -ovens")
+        os.system("echo custom kitchen deliveries")
+        os.system("move refrigerators")
+        os.system("move color -tvs")
+             
+      except:
+        print("Can't work")
+        
+      
+      finally:
+        print("🎵🎵🎵")
+                
+
+if __name__ == '__main__':
+  song = MTV().play()
+   
+
+  
